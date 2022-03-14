bec0a9b454dca1a8935b1064de6929aecf9cffda
obi-wan <cayton10@live.marshall.edu>
2022-03-13 22:07:07 -0400

    Block Letters Exercise

---
+++ Block_Letters/block-letters.py
@@ -0,0 +1,14 @@
+# Ben Cayton
+# I like creating digital art and hope to find more time to do it after I'm better at my engineering / development job
+
+initials = """
+BBBB    CCC
+B   B  C   C
+B  B   C
+BBBB   C
+B   B  C
+B   B  C   C
+BBBB    CCC
+"""
+
+print(initials)
---
+++ README.md
@@ -0,0 +1,3 @@
+# CS with Python
+
+This repo is for storing my work of using python to review CS concepts from the codecademy Computer Science Career Path
